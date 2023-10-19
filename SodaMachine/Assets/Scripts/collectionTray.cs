6cefe2bee0f1f739bf9b5bbeda87cd5d27bbb1c4
Mary <marykita.contact@gmail.com>
2023-10-19 21:23:00 +0200

    test

--- SodaMachine/Assets/Scripts/collectionTray.cs
+++ SodaMachine/Assets/Scripts/collectionTray.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class collectionTray : MonoBehaviour
-{
-    public GameObject drinks;
+{	
+	public GameObject drinks;
+	public AudioSource source;
+    public AudioClip clip;
+	
     private void OnTriggerEnter(Collider drinks)
     {
         Debug.Log("La boisson est bien dans le bac de récupération");
         drinks.GetComponent<CanScript>().canMove = false;
+		source.PlayOneShot(clip);
     }
 
 
